ec19335bf6a436cff93c59c249229bbe176328d5
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-11-11 23:44:26 +0700

    feat: add theme toggle functionality with dark/light mode support
    - Added `.air.toml` for Air tool configuration to auto-reload Go code during development.
    - Updated `.gitignore` to ignore `/tmp` directory.
    - Modified route handling in `routes.go` to allow static files under `/static/*` path.
    - Added theme toggle button in `base.layout.tmpl` for switching between light and dark themes.
    - Implemented theme switching logic with persistent user preference using localStorage.
    - Styled the theme toggle button and implemented hover effects.
    - Created `base.css` to define light/dark mode styles and custom CSS variables.
    - Positioned the theme toggle button at the top-right of the header.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,7 +8,7 @@ import (
 
 func (app *application) routes(r *chi.Mux) {
 	fs := http.FileServer(http.Dir("./ui/static"))
-	r.Handle("/static/", http.StripPrefix("/static", fs))
+	r.Handle("/static/*", http.StripPrefix("/static", fs))
 	r.Route("/", func(r chi.Router) {
 		r.Get("/login", app.loginPage)
 	})
